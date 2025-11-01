2dd42d6dd397bbde36fe60ff42c4523149c1b466
Pervertcoder <lance60702@gmail.com>
2025-11-01 21:53:36 +0800

    debug Task 1 part 1

--- week_three/catch.py
+++ week_three/catch.py
@@ -20,113 +20,91 @@ def disconstructin (lis):
          empty_list.append(j)
     return empty_list
 
+# 分隔元素
+def divide_element(lis, num):
+    new_result = []
+    for i in range(0, len(lis), num):
+        new_result.append(lis[i:i + num])
+    return new_result
+
+# 找英文名字
+def find_index_id (lis, n):
+    for i in hotel_EN:
+        if i['_id'] == lis[n][0]:
+            index = hotel_EN.index(i)
+    return hotel_EN[index]['hotel name']
+
+# 找英文地址
+def find_index_id_adress (lis, n):
+    for i in hotel_EN:
+        if i['_id'] == lis[n][0]:
+            index = hotel_EN.index(i)
+    return hotel_EN[index]['address']
+
+# ID編碼
+id_list = []
+for i in hotel_CH:
+    hotel_id = i['_id']
+    id_list.append(hotel_id)
+# print(id_list)
 
 #　中文名字
 hotel_CH_name_list = [] 
 for i in hotel_CH:
     hotel_CH_name = i['旅宿名稱']
     hotel_CH_name_list.append(hotel_CH_name)
-
 # print(hotel_CH_name_list)
 
+hotel_id_name = list(zip(id_list, hotel_CH_name_list))
+# print(hotel_id_name)
+hotel_id_name_list = disconstructin(hotel_id_name)
+hotel_id_name1 = divide_element(hotel_id_name_list, 2)
+# print(hotel_id_name1)
 
-# 英文名字
-hotel_EN_name_list = [] 
-for i in hotel_EN:
-    hotel_EN_name = i['hotel name']
-    hotel_EN_name_list.append(hotel_EN_name)
+# 加入英文名字
+for i in hotel_id_name1:
+    order = hotel_id_name1.index(i)
+    engname_order = find_index_id(hotel_id_name1, order)
+    i.append(engname_order)
+# print (hotel_id_name1)
 
-# print(hotel_EN_name_list)
+# 加入中文地址
+for h in range(len(hotel_id_name1)):
+    hotel_CH_address = hotel_CH[h]['地址']
+    hotel_id_name1[h].append(hotel_CH_address)
 
+# 加入英文地址
+for j in hotel_id_name1:
+    order = hotel_id_name1.index(j)
+    engaddress = find_index_id_adress(hotel_id_name1, order)
+    j.append(engaddress)
 
-# 中英名字混和
-name_CH_EN_list = list(zip(hotel_CH_name_list, hotel_EN_name_list)) 
+# 加入電話
+for k in range(len(hotel_id_name1)):
+    hotel_phone = hotel_CH[k]['電話或手機號碼']
+    hotel_id_name1[k].append(hotel_phone)
 
+# 加入房間數
+for l in range(len(hotel_id_name1)):
+    hotel_room = hotel_CH[l]['房間數']
+    hotel_id_name1[l].append(hotel_room)
+# print(hotel_id_name1)
 
-# 中文地址
-hotel_address_CH_list = []
-for i in hotel_CH:
-    hotel_addrss_CH = i['地址']
-    hotel_address_CH_list.append(hotel_addrss_CH)
-# print(hotel_address_CH_list)
-
-
-# 英文地址
-hotel_address_EN_list = []
-for i in hotel_EN:
-    hotel_addrss_EN = i['address']
-    hotel_address_EN_list.append(hotel_addrss_EN)
-# print(hotel_address_EN_list)
-
-
-# 中英混和地址
-adress_CH_EN_list = list(zip(hotel_address_CH_list, hotel_address_EN_list))
-# print(adress_CH_EN_list)
-
-
-middle_result = list(zip(name_CH_EN_list, adress_CH_EN_list))
-# print(middle_result)
-
-# 手機
-hotel_telephone = []
-for i in hotel_CH:
-    hotel_telephone_CH = i['電話或手機號碼']
-    hotel_telephone.append(hotel_telephone_CH)
-# print (hotel_telephone)
-
-
-# 房間數
-hotel_room = []
-for i in hotel_CH:
-    hotel_room_CH = i['房間數']
-    hotel_room.append(hotel_room_CH)
-# print(hotel_room)
-
-# 手機+房間數
-telephone_room = list(zip(hotel_telephone, hotel_room))
-# print(telephone_room)
-
-
-middle_result2 = list(zip(middle_result, telephone_room))
-# print(middle_result2)
-
-
-a = disconstructin(middle_result2)
-b = disconstructin(a)
-# print(b)
-
-
-# 展開全部的元素
-result = []
-for i in b:
-    if type(i) == tuple:
-        for j in i:
-            result.append(j)
-    else:
-        result.append(i)
-# print(result)
-
-
-# 讓每六個元素組成一個list
-new_result = []
-second_layer = []
-for i in range(0, len(result), 6):
-    new_result.append((result[i:i+6]))
-# print(new_result)
-
-
+for w in hotel_id_name1:
+    del w[0]
+print(hotel_id_name1)
 
 # 行政區
 district = []
-for area in new_result:
+for area in hotel_id_name1:
     small_area = area[2][3:6]
     district.append(small_area)
     # print(district)
 
 
-# 房間數
+# # 房間數
 room_count = []
-for room in new_result:
+for room in hotel_id_name1:
     room_count.append(room[5])
     # print(room_count)
 
@@ -135,22 +113,22 @@ district_room_count = list(zip(district, room_count))
 
 taipei_district = ['北投區', '士林區', '內湖區', '南港區', '文山區', '大同區', '中山區', '松山區', '信義區', '大安區', '中正區', '萬華區']
 
-# district_1 = []
-# for i in district_room_count:
-#     if i[0] == taipei_district[0]:
-#         district_1.append(i)
+district_1 = []
+for i in district_room_count:
+    if i[0] == taipei_district[0]:
+        district_1.append(i)
 
-# hotel_num = len(district_1)
+hotel_num = len(district_1)
 
-# district_1_hotel_room = []
-# for i in district_1:
-#     district_1_hotel_room.append(i[1])
+district_1_hotel_room = []
+for i in district_1:
+    district_1_hotel_room.append(i[1])
 
-# sum_district_1_hotel_room = 0
-# for i in district_1_hotel_room:
-#     sum_district_1_hotel_room += int(i)
+sum_district_1_hotel_room = 0
+for i in district_1_hotel_room:
+    sum_district_1_hotel_room += int(i)
 
-# D1 = [[taipei_district[0], hotel_num, sum_district_1_hotel_room]]
+D1 = [[taipei_district[0], hotel_num, sum_district_1_hotel_room]]
 # print(D1)
 
 def district_maker(lis, dis):
@@ -181,17 +159,17 @@ for i in taipei_district:
 print (district_result)
 
 
-# csv檔案寫入
-file = open('hotel.csv', mode='w', newline='', encoding='utf-8-sig')
-writer = csv.writer(file)
-for i in new_result:
-    writer.writerow(i)
-file.close()
+# # csv檔案寫入
+# file = open('hotel.csv', mode='w', newline='', encoding='utf-8-sig')
+# writer = csv.writer(file)
+# for i in hotel_id_name1:
+#     writer.writerow(i)
+# file.close()
 
 
-# csv檔案寫入
-file = open('district.csv', mode='w', newline='', encoding='utf-8-sig')
-writer = csv.writer(file)
-for i in district_result:
-    writer.writerow(i)
-file.close()
+# # csv檔案寫入
+# file = open('district.csv', mode='w', newline='', encoding='utf-8-sig')
+# writer = csv.writer(file)
+# for i in district_result:
+#     writer.writerow(i)
+# file.close()
